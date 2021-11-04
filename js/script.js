f0118233bd0670bc8c18edb3d3821a04a0b24c0f
Luca Sunseri <89150843+LucaSunseri@users.noreply.github.com>
2021-11-04 13:39:52 +0100

    add array memberTeams

---
+++ js/script.js
@@ -0,0 +1,32 @@
+const membersTeam = [
+  {
+    photo: 'img/wayne-barnett-founder-ceo.jpg',
+    name: 'Wayne Barnett',
+    role: 'Chief Editor',
+  },
+  {
+    photo: 'img/angela-caroll-chief-editor.jpg',
+    name: 'Angela Croll',
+    role: 'Chief Editor',
+  },
+  {
+    photo: 'img/walter-gordon-office-manager.jpg',
+    name: 'Walter Gordon',
+    role: 'Office Manager',
+  },
+  {
+    photo: 'img/angela-lopez-social-media-manager.jpg',
+    name: 'Angela Lopez',
+    role: 'Social Media Manager',
+  },
+  {
+    photo: 'img/scott-estrada-developer.jpg',
+    name: 'Scott Estrada',
+    role: 'Developer',
+  },
+  {
+    photo: 'img/barbara-ramos-graphic-designer.jpg',
+    name: 'Barbara Ramos',
+    role: 'Graphic Designer',
+  }
+]
