93fe99e0c88e64ac88890d54c2eb903bfc0fa8f5
Luca Sunseri <89150843+LucaSunseri@users.noreply.github.com>
2021-11-04 14:31:05 +0100

    Created all the cards and append in html

--- js/script.js
+++ js/script.js
@@ -30,3 +30,51 @@ const membersTeam = [
     role: 'Graphic Designer',
   }
 ]
+// console.log('Vediamo il nostro array di oggetti',membersTeam);
+
+const teamContainer = document.querySelector('.team-container');
+teamContainer.innerHTML = '';
+
+
+
+for (let key in membersTeam) {
+  // console.log(membersTeam[key]);
+
+  const member = membersTeam[key];
+
+  let photo = member.photo;
+  let name = member.name;
+  let role = member.role;
+
+  console.log('Foto',photo);
+  console.log('Nome',name);
+  console.log('Ruolo',role);
+
+  createCard(photo, name, role);
+  
+}
+
+function createCard(photo, name, role) {
+
+  const teamCard = document.createElement('div');
+  teamCard.className = 'team-card';
+  teamContainer.append(teamCard);
+
+  photo;
+  name;
+  role,
+
+  teamCard.innerHTML = `
+  <div class="card-image">
+  <img
+    src="${photo}"
+    alt="Wayne Barnett"
+  />
+  </div>
+  <div class="card-text">
+    <h3>${name}</h3>
+    <p>${role}</p>
+  </div>
+  `;
+}
+
